be4eb19af3c4bdc8aac23b7161180b5bf4e55fb3
Martin Bergemann <martin.bergemann@monash.edu>
2018-04-25 22:18:16 +1000

    Added monash.users space

---
+++ javascript/slideshow.js
@@ -0,0 +1,55 @@
+//var slideIndex = 1;
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var slideIndex = getRandomIntInclusive(1,6)
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+
+
+//function showSlides(n) {
+//  var i;
+// var slides = document.getElementsByClassName("mySlides");
+//  var dots = document.getElementsByClassName("dot");
+//  if (n > slides.length) {slideIndex = 1}    
+//  if (n < 1) {slideIndex = slides.length}
+//  for (i = 0; i < slides.length; i++) {
+//      slides[i].style.display = "none";  
+//  }
+//  for (i = 0; i < dots.length; i++) {
+//      dots[i].className = dots[i].className.replace(" active", "");
+//  }
+//  slides[slideIndex-1].style.display = "block";  
+//  dots[slideIndex-1].className += " active";
+//}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+    if (slideIndex> slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 20000); // Change image every 2 seconds
+}
